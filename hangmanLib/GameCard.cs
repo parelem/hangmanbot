47fd80625f7fcafa161d9ca57b7ff75008e224a0
julie.gierald <julie.gierald@gmail.com>
2017-10-02 16:45:50 -0500

    initial commit

---
+++ hangmanLib/GameCard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace hangmanLib
+{
+    public enum Status
+    {
+        InProgress,
+        Won,
+        Lost
+    }
+    public class GameCard
+    {
+        public string Word { get; set; }
+        public int Lives { get; set; } = 5;
+        public List<char> Guesses { get; set; }  = new List<char>();
+        public string CurrentWord { get; set; }
+        public Status Status { get; set; } = Status.InProgress;
+    }
+}
---
+++ hangmanLib/HangMan.cs
@@ -0,0 +1,84 @@
+﻿using RestSharp;
+
+namespace hangmanLib
+{
+    public class HangMan
+    {
+        public GameCard Game { get; set; }
+
+        public HangMan()
+        {
+            Start();
+        }
+
+        private void Start()
+        {
+            GameCard newGame = new GameCard();
+           // newGame.Word = GetWord();
+            //newGame.CurrentWord = new string('_', newGame.Word.Length);
+            Game = newGame;
+        }
+
+        public bool GetWord()
+        {
+            bool retVal = false;
+            
+            RestClient client = new RestClient("http://randomword.setgetgo.com/get.php");
+            RestRequest request = new RestRequest {Method = Method.GET};
+            IRestResponse response = client.Execute(request);
+            var word = response.Content;
+            Game.Word = word.ToUpper();
+            Game.CurrentWord = new string('@', Game.Word.Length);
+            
+            return retVal;
+        }
+
+        public string Pretty()
+        {
+            //return Game.CurrentWord.Aggregate("", (current, c) => current + (c + " "));
+            return Game.CurrentWord;
+        }
+
+        public int GuessLetter(char letter)
+        {
+            int correct = -1;
+
+            string tempLetter = letter.ToString().ToUpper();
+
+            if (Game.Guesses.Contains(letter))
+            {
+                return correct;
+            }
+            Game.Guesses.Add(letter);
+
+            if (Game.Word.Contains(tempLetter))
+            {
+                correct = 1;
+
+                for (int i = 0; i < Game.Word.Length; i++)
+                {
+                    if (Game.Word[i].ToString().ToUpper().Equals(tempLetter))
+                    {
+                        Game.CurrentWord = Game.CurrentWord.Remove(i, 1).Insert(i, tempLetter);
+                    }    
+                }
+
+                if (Game.CurrentWord.Equals(Game.Word))
+                {
+                    Game.Status = Status.Won;
+                }
+            }
+            else
+            {
+                correct = 0;
+                Game.Lives --;
+                if (Game.Lives == 0)
+                {
+                    Game.Status = Status.Lost;
+                }
+            }
+
+            return correct;
+        }
+    }
+}
